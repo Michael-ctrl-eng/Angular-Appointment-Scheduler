9c90e0a1c1c8a0b6042964077fe378f6b7a2ebeb
Michael <michael.makram.zm@gmail.com>
2025-02-01 12:56:36 +0200

    Create angular-material.module.ts

---
+++ shared/angular-material.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCardModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatBadgeModule,
+  MatTabsModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  DragDropModule,
+  MatMenuModule,
+  MatTooltipModule,
+  MatSlideToggleModule,
+  MatRadioModule
+];
+
+@NgModule({
+  imports: materialModules,
+  exports: materialModules
+})
+export class AngularMaterialModule { }
