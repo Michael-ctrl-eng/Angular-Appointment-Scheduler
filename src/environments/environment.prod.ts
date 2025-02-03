8aebc2ff7494becb000b6a773ebbe1f94ee2469c
Michael <michael.makram.zm@gmail.com>
2025-02-03 21:26:49 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,4 @@
-
+export const environment = {
+  production: true,
+  apiUrl: '/api' // Example for production -  Adjust to your actual production API base URL (e.g., 'https://your-api-domain.com/api')
+};
